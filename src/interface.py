96b0df98653e198502a362a5d55ea879c2ef7fdf
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-03-13 14:43:19 -0500

    I don't even know anymore xD

--- src/interface.py
+++ src/interface.py
@@ -1,14 +1,18 @@
 import socket
-import src.logging
+import logger from src.logging
 import sys
 import time
 
-class interface:
-  
-def connection(ip = "0.0.0.0", port = 19132):
-  s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  bind = s.bind(ip, port)
-  if not bind:
-    logging.warning("Couldn't bind to "+ ip +":"+port)
-    time.sleep(5)
-    sys.exit("process eliminated.")
+class interface(object):
+	"""docstring for interface"""
+	def __init__(self, arg):
+		super(interface, self).__init__()
+		self.arg = arg
+
+		def connection(ip = "0.0.0.0", port = 19132):
+			s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			bind = s.bind(ip, port)
+			if not bind:
+				logging.warning("Couldn't bind to "+ ip +":"+port)
+				time.sleep(5)
+				sys.exit("Process Eliminated.")
