fafbb38b29e566e08e60323c5d483413f965786e
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-03-13 15:03:17 -0500

    Update logging.py

--- src/logging.py
+++ src/logging.py
@@ -1,20 +1,24 @@
 import logging
 
 class logger(object):
+	"""docstring for logger"""
+	def __init__(self, arg):
+		super(logger, self).__init__()
+		self.arg = arg
 
-def info(self, message):
-  logging.info("[INFO] "+message)
-  
-def warning(self, message):
-  logging.warning("[WARNING] "+message)
-  
-def error(self, message):
-  logging.error("[ERROR] "+message)
-  
-def debug(self, message):
-  logging.debug("[DEBUG] "+message)
-  
-def critical(self, message)
-  logging.critical("[CRITICAL] "+message)
-  
-  logger = logger()
+	def info(self, message):
+		logging.info("[INFO] "+message)
+
+	def warning(self, message):
+		logging.warning("[INFO] "+message)
+
+	def errors(self, message):
+		logging.error("[ERROR] "+message)
+
+	def debug(self, message):
+		logging.debug("[DEBUG] "+message)
+
+	def critical(self, message):
+		logging.critical("[CRITICAL] "+message)
+
+logger = logger()
