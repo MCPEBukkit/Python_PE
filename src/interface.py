fc0474f8fe1607739ec1c84d8cbe8230bf8cfb03
I_Is_Payton_ <IPaytonn@users.noreply.github.com>
2014-03-13 14:31:37 -0500

    LOL Used to php xD

--- src/interface.py
+++ src/interface.py
@@ -8,7 +8,7 @@ class interface:
 def connection(ip = "0.0.0.0", port = 19132):
   s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
   bind = s.bind(ip, port)
-  if(!bind):
+  if not bind:
     logging.warning("Couldn't bind to "+ ip +":"+port)
     time.sleep(5)
     sys.exit("process eliminated.")
